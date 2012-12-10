17bb4db5de79e422eb7e734d3f20248801fe9bed
Adam.Nathan <adamnathan@Adam-Nathans-MacBook-Pro.local>
2012-12-10 10:09:51 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+      name      "Adam Nathan"
+      email     "adam.r.nathan@gmail.com"
+      password  "123456"
+      password_confirmation "123456"
+  end
+end
